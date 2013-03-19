77980fae5cdef4938ea3a980220d0c89c3b24360
funnythingz <hiroki@hackleberry.me>
2013-03-20 02:08:36 +0900

    added Images

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ http_path = "/"
 sass_options = (environment == :production) ? {:debug_info => false} : {:debug_info => true}
 css_dir = (environment == :production) ? "css" : "css_debug"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "imgs"
 javascripts_dir = "js"
 
 output_style = (environment == :production) ? :compressed : :expanded
