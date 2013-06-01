5916363277702e9f226d8ef6da0a6b43ad3f18d1
funnythingz <hiroki@hackleberry.me>
2013-06-01 14:22:36 +0900

    updated

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 http_path = "/"
-sass_options = (environment == :production) ? {:debug_info => false} : {:debug_info => true}
+#sass_options = (environment == :production) ? {:debug_info => false} : {:debug_info => true}
 css_dir = (environment == :production) ? "css" : "css_debug"
 sass_dir = "sass"
 images_dir = "imgs"
